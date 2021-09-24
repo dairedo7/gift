2e1ee72fae2708dc8f4fcaa5d04b9dc43fea2de4
dairedo7 <59849624+dairedo7@users.noreply.github.com>
2021-09-24 11:05:04 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,3 +13,49 @@
     document.body.classList.toggle("modal-open");
   }
 })();
+
+var record = document.getElementById("record").autoplay;
+record.autoplay = true;
+
+var source = "record.mp3";
+var audio = new Audio(); // use the constructor in JavaScript, just easier that way
+audio.addEventListener("load", function() {
+  audio.play();
+}, true);
+audio.src = source;
+audio.autoplay = true; // add this
+
+$("#playBtn").click(function() {
+  audio.play();
+});
+
+$("#pauseBtn").click(function() {
+  audio.pause();
+});
+
+$("#stopBtn").click(function() {
+  audio.pause();
+  audio.currentTime = 0;
+});
+
+
+
+// audioElement = new Audio('record.mp3');
+// audioElement.addEventListener("canplay", function() {
+//   audioElement.play();
+// }, false);
+// audioElement.addEventListener('ended', function () {
+//         this.currentTime = 0;
+//         this.play();
+// }, false);
+
+// $('#music').on('click', function(e) {
+//     e.preventDefault();
+//     var volIcon = $(this).find('i');
+//     volIcon.toggleClass('fa-volume-up fa-volume-off');
+//     if (volIcon.hasClass('fa-volume-off')) {
+//         audioElement.pause();
+//     } else {
+//         audioElement.play();
+//     }
+// });
